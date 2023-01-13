33661fd389414afebd902fdfb4769f0e14682dc0
Charles Titus <ctitus@bnl.gov>
2023-01-13 10:58:16 -0500

    TST: added test that passing in any custom per_step, even one that uses primary, causes strict checking of pre-declaration to fail

--- bluesky/tests/test_new_examples.py
+++ bluesky/tests/test_new_examples.py
@@ -733,3 +733,6 @@ def test_custom_stream_name(RE, hw):
 
     with pytest.raises(IllegalMessageSequence):
         RE(count([hw.det], 3, per_shot=new_per_shot))
+
+    with pytest.raises(IllegalMessageSequence):
+        RE(count([hw.det], 3, per_shot=one_shot))
